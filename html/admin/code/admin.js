19f66e33c4a2a5bfac61f9c9ad58a922c52d25f2
Bas Wijnen <wijnen@debian.org>
2021-01-17 12:24:48 +0100

    complete edit interface

--- html/admin/code/admin.js
+++ html/admin/code/admin.js
@@ -6,6 +6,149 @@ function reset_password(user) {
 	server.call('reset_password', [user]);
 }
 
+function button(label, click, items) {
+	var ret = Create('button');
+	ret.type = 'button';
+	ret.AddText(label);
+	ret.AddEvent('click', function() { click.call(this); });
+	if (items) {
+		for (i in items)
+			ret[i] = items[i];
+	}
+	return ret;
+}
+
+function EditableList(data, cbs) {
+	// This is a class which provides an editable list.
+	// data is an array which contains the rows.
+	// data is updated when editing.
+	// cbs contains callbacks:
+	//	add is a callback to handle a click on the "add" button.
+	//	create is a callback to create one element in the list.
+	//	edit is a callback to start editing. If undefined, the button is not shown.
+	// The return value is an Element which should be inserted into the DOM.
+	var ret = Create('table');
+	ret.options = [];
+	var final_tr = ret.AddElement('tr');
+	var td = final_tr.AddElement('td');
+	td.colSpan = cbs['edit'] ? 5 : 4;
+	td.Add(button('➕', function() {
+		cbs.add(data);
+	}, {'className': 'list_add'}));
+	ret.add = function(content, idx) {
+		// Add an item to the list. If idx is undefined, also add it to data.
+		if (idx === undefined) {
+			idx = data.length;
+			data.push(content);
+		}
+		var tr = Create('tr');
+		ret.insertBefore(tr, final_tr);
+		ret.options.push(tr);
+		if (idx == 0)
+			tr.AddClass('first');
+		else
+			ret.options[idx - 1].RemoveClass('last');
+		tr.AddClass('last');
+		if (idx & 1)
+			tr.AddClass('odd');
+		tr.td = tr.AddElement('td');
+		tr.td.idx = idx;
+		cbs.create(tr.td, content);
+		tr.AddElement('td').Add(button('❌', function() {
+			cbs.remove(tr.td, idx, content);
+		}));
+		tr.AddElement('td').Add(button('↑', function() {
+			cbs.up(tr.td, idx, content);
+		}, {'className': 'up'}));
+		tr.AddElement('td').Add(button('↓', function() {
+			cbs.down(tr.td, idx, content);
+		}, {'className': 'down'}));
+		if (cbs.edit != undefined) {
+			tr.AddElement('td').Add(button('✎', function() {
+				cbs.edit(tr.td, idx, content);
+			}));
+		}
+	};
+	for (var i = 0; i < data.length; ++i)
+		ret.add(data[i], i);
+	ret.set = function(obj, value) {
+		// Set value in data which corresponds to obj to value.
+		data[obj.idx] = value;
+	};
+	ret.get = function(obj) {
+		// Get value in data which corresponds to obj.
+		return data[obj.idx];
+	};
+	ret.remove = function(obj) {
+		// Remove item idx from the list. Also remove it from data.
+		var idx = obj.idx;
+		ret.removeChild(ret.options.splice(idx, 1));
+		data.splice(idx, 1);
+		for (var i = idx; i < data.length; ++i) {
+			ret.options[i].idx = i;
+			if (i & 1)
+				ret.options[i].AddClass('odd');
+			else
+				ret.options[i].RemoveClass('odd');
+		}
+		ret.options[0].AddClass('first');
+		ret.options[ret.options.length - 1].AddClass('last');
+	};
+	ret.up = function(obj) {
+		var idx = obj.idx;
+		console.assert(idx > 0);
+		var item = ret.options.splice(idx, 1);
+		ret.removeChild(item);
+		ret.insertBefore(item, ret.options[idx - 1]);
+		data.splice(idx - 1, 0, data.splice(idx, 1));
+		ret.options[idx].idx = idx;
+		ret.options[idx - 1].idx = idx - 1;
+		if (idx & 1) {
+			ret.options[idx].AddClass('odd');
+			ret.options[idx - 1].RemoveClass('odd');
+		}
+		else {
+			ret.options[idx].RemoveClass('odd');
+			ret.options[idx - 1].AddClass('odd');
+		}
+		if (idx == 1) {
+			ret.options[0].AddClass('first');
+			ret.options[1].RemoveClass('first');
+		}
+		if (idx == ret.options.length - 1) {
+			ret.options[idx].AddClass('last');
+			ret.options[idx - 1].RemoveClass('last');
+		}
+	};
+	ret.down = function(obj) {
+		var idx = obj.idx;
+		console.assert(idx < data.length - 1);
+		var item = ret.options.splice(idx, 1);
+		ret.removeChild(item);
+		ret.insertBefore(item, ret.options[idx + 1]);
+		data.splice(idx, 0, data.splice(idx, 1));
+		ret.options[idx].idx = idx;
+		ret.options[idx + 1].idx = idx + 1;
+		if (idx & 1) {
+			ret.options[idx].AddClass('odd');
+			ret.options[idx + 1].RemoveClass('odd');
+		}
+		else {
+			ret.options[idx].RemoveClass('odd');
+			ret.options[idx + 1].AddClass('odd');
+		}
+		if (idx == 0) {
+			ret.options[0].AddClass('first');
+			ret.options[1].RemoveClass('first');
+		}
+		if (idx == ret.options.length - 2) {
+			ret.options[idx].AddClass('last');
+			ret.options[idx - 1].RemoveClass('last');
+		}
+	};
+	return ret;
+}
+
 function build_content() {
 	// Build group selection.
 	var list = document.getElementById('students');
@@ -46,80 +189,91 @@ function build_content() {
 		}
 	}
 	// Build question selection.
-	var table = document.getElementById('questions');
-	table.ClearAll();
+	var qdiv = document.getElementById('questions');
+	qdiv.ClearAll();
 	if (program !== undefined) {
-		for (var p = 0; p < program.length; ++p) {
-			var tr = table.AddElement('tr');
-			if (p & 1)
-				tr.AddClass('odd');
-			var input = tr.AddElement('td').AddElement('input');
-			input.type = 'radio';
-			input.idx = p;
-			input.checked = p == current;
-			input.AddEvent('click', function() {
-				event.preventDefault();
-				server.call('run', [this.idx]);
-			});
-			input.id = 'q' + p;
-			input.name = 'question';
-			var label = tr.AddElement('td').AddElement('label');
-			label.htmlFor = input.id;
-			label.innerHTML = program[p].arg;
-			var td = tr.AddElement('td');
-			if (p > 0) {
-				var button = td.AddElement('button').AddText('↑').AddEvent('click', function() {
-					server.call('q_move_up', [this.idx]);
+		qdiv.Add(EditableList(program, {
+			'remove': function(parent, idx, data) { server.call('q_remove', [parent.idx]) },
+			'up': function(parent, idx, data) { server.call('q_move_up', [parent.idx]) },
+			'down': function(parent, idx, data) { server.call('q_move_up', [parent.idx]) },
+			'add': function(data) { server.call('new_question'); },
+			'create': function(parent, data) {
+				var input = parent.AddElement('input');
+				input.type = 'radio';
+				input.checked = parent.idx == current;
+				input.AddEvent('click', function() {
+					event.preventDefault();
+					server.call('run', parent.idx);
 				});
-				button.idx = p;
-			}
-			if (p < program.length - 1) {
-				button = td.AddElement('button').AddText('↓').AddEvent('click', function() {
-					server.call('q_move_down', [this.idx]);
-				});
-				button.idx = p;
-			}
-			button = td.AddElement('button').AddText('❌').AddEvent('click', function() {
-				server.call('q_remove', [this.idx]);
-			});
-			button.idx = p;
-			button = td.AddElement('button').AddText('✏').AddEvent('click', function() {
-				if (this.editing) {
-					server.call('edit', [this.idx, this.box.value]);
+				input.id = 'q' + parent.idx;
+				input.name = 'question';
+				parent.body = parent.AddElement('span');
+				parent.body.innerHTML = data.arg;
+				parent.body.title = data.title;
+				parent.editing = false;
+			},
+			'edit': function(parent, idx, data) {
+				// Edit question.
+				if (parent.editing) {
+					server.call('q_edit', [idx, parent.typeselect.options[parent.typeselect.selectedIndex].value, parent.casesensitive.checked, parent.box.value, data.option]);
 				}
 				else {
-					this.label.ClearAll();
-					this.type = this.label.AddElement('select');
+					parent.body.ClearAll();
+					parent.typeselect = parent.body.AddElement('select');
 					var types = ['Title', 'Term', 'Terms', 'Word', 'Words', 'Choice', 'Choices'];
 					for (var o = 0; o < types.length; ++o) {
-						var option = this.type.AddElement('option');
+						var option = parent.typeselect.AddElement('option');
 						option.value = types[o];
+						option.AddText(types[o]);
+						if (data.cmd == types[o].toLowerCase())
+							option.selected = true;
 					}
-					var clabel = this.label.AddElement('label');
-					this.casesensitive = this.clabel.AddElement('input');
-					this.casesensitive.type = 'checkbox';
-					this.clabel.AddText('Case Sensitive');
-					this.box = this.label.AddElement('textarea');
-					this.update = function() {
-						var mc = this.type.options[this.type.selectedIndex].value[0] == 'C';
-						if (mc) {
-							this.box.style.display = 'none';
-							// TODO
+					var clabel = parent.body.AddElement('label');
+					parent.casesensitive = clabel.AddElement('input');
+					parent.casesensitive.type = 'checkbox';
+					parent.casesensitive.checked = data['case'];
+					clabel.AddText('Case Sensitive');
+					parent.box = parent.body.AddElement('textarea');
+					parent.box.value = data.markdown;
+					if (data.option === undefined)
+						data.option = [];
+					parent.optlist = EditableList(data.option, {
+						'add': function(data) {
+							parent.optlist.add('');
+						},
+						'create': function(parent2, data) {
+							var input = parent2.AddElement('input');
+							input.type = 'text';
+							input.value = data;
+							input.AddEvent('change', function() {
+								parent.optlist.set(parent2, input.value);
+							});
+							input.focus();
+						},
+						'up': function(item, idx, data) {
+							parent.optlist.up(idx);
+						},
+						'down': function(item, idx, data) {
+							parent.optlist.down(idx);
+						},
+						'remove': function(item, idx, data) {
+							parent.optlist.remove(idx);
 						}
-						else {
-							this.box.style.display = '';
-							this.box.value = program[this.idx].markdown;
-						}
-						this.editing = true;
+					});
+					parent.Add(parent.optlist);
+					parent.update = function() {
+						var mc = parent.typeselect.options[parent.typeselect.selectedIndex].value[0] == 'C';
+						if (mc)
+							parent.optlist.style.display = '';
+						else
+							parent.optlist.style.display = 'none';
+						parent.editing = true;
 					};
-					this.type.AddEvent('change', this.update);
-					this.update();
+					parent.typeselect.AddEvent('change', parent.update);
+					parent.update();
 				}
-			});
-			button.idx = p;
-			button.label = label;
-			button.editing = false;
-		}
+			}
+		}));
 	}
 	// Build response section.
 	var table = document.getElementById('results');
@@ -137,19 +291,17 @@ function build_content() {
 				var button = tr.AddElement('td').AddElement('button').AddText(responses[r].state);
 				button.idx = r;
 				button.type = 'button';
+				button.AddClass(responses[r].state);
 				button.AddEvent('click', function() {
 					reset_password(this.idx);
 				});
 			}
 			else
-				tr.AddElement('td').AddText(responses[r].state);
+				tr.AddElement('td').AddText(responses[r].state).AddClass(responses[r].state);
 			var use = tr.AddElement('td').AddElement('input');
 			use.type = 'checkbox';
 			use.checked = !responses[r].blocked;
 			use.idx = r;
-			use.AddEvent('change', function() {
-				console.info(responses[this.idx], this.checked);
-			});
 			tr.AddElement('td').AddText(responses[r].answer);
 		}
 	}
@@ -204,8 +356,8 @@ var Connection = {
 		build_content();
 	},
 	cookie: function(n, c) {
-		document.cookie = 'name=' + encodeURIComponent(n);
-		document.cookie = 'key=' + encodeURIComponent(c);
+		document.cookie = 'name=' + encodeURIComponent(n) + '; sameSite=strict';
+		document.cookie = 'key=' + encodeURIComponent(c) + '; sameSite=strict';
 	},
 };
 
